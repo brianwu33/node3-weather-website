c0630037d875c60bca0fdbcc94d062ea862d95ac
brianwu33 <brianwu20020303@gmail.com>
2022-03-20 16:31:01 -0400

    Add Humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,5 @@
 const request = require("request");
 
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
 const forecast = (latitude, longitude, callback) => {
 	const url =
 		"http://api.weatherstack.com/current?access_key=3eaf7c6f854886df97757087aeb1f9c9&query=" +
@@ -30,7 +21,10 @@ const forecast = (latitude, longitude, callback) => {
 					body.current.temperature +
 					" degrees, and it feels like " +
 					body.current.feelslike +
-					" degrees."
+					" degrees." +
+					"The humidity is " +
+					body.current.humidity +
+					"%."
 			);
 		}
 	});
